165e948f8667c7dab08031dfe149569ebde91a0a
Szabó András <szabag@gmail.com>
2019-04-24 18:30:15 +0200

    working MLP, accuracy over 99

--- hand-recognizer-nn.py
+++ hand-recognizer-nn.py
@@ -1,43 +1,54 @@
 import os
 import time
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 from keras import Sequential
 from keras.layers import Dense
-from keras.regularizers import l2
 
-data = np.loadtxt("data/poker-hand-training-true.data", delimiter=",")
+print("Reading data sources")
+train_data_source = np.loadtxt("data/poker_hand_extended_train.data", delimiter=",")
+test_data_source = np.loadtxt("data/poker_hand_extended_train.data", delimiter=",")
 nb_classes = 10
 
+random.shuffle(train_data_source)
+random.shuffle(test_data_source)
+
 data_distribution = 0.8
-train_length = round(data_distribution * len(data))
-validation_length = round(len(data) - train_length)
+train_length = round(data_distribution * len(train_data_source))
+validation_length = round(len(train_data_source) - train_length)
 print("%d train and %d validation data" % (train_length, validation_length))
 
-train_data = data[:train_length,:10]
-train_label = data[:train_length,10]
+train_data = train_data_source[:train_length, :10]
+train_label = train_data_source[:train_length, 10]
 
-validation_data = data[train_length:, :10]
-validation_label = data[train_length:, 10]
+validation_data = train_data_source[train_length:, :10]
+validation_label = train_data_source[train_length:, 10]
 
-# Our first Keras Model
+test_data = test_data_source[:, :10]
+test_labels = test_data_source[:, 10]
+
 model = Sequential()
-model.add(Dense(1000, input_shape=(10,), activation='relu'))
-model.add(Dense(2000, activation='relu'))
-model.add(Dense(1000, activation='relu'))
+model.add(Dense(200, input_shape=(10,), activation='relu'))
+model.add(Dense(400, activation='relu'))
+model.add(Dense(200, activation='relu'))
 model.add(Dense(nb_classes, activation='softmax'))
 model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print("Model created, training started")
 
-history = model.fit(train_data, train_label, validation_split=0.33, shuffle=True, epochs=100, verbose=0, batch_size=32)
+history = model.fit(train_data, train_label,
+                    batch_size=32,
+                    epochs=30,
+                    validation_data=(validation_data, validation_label),
+                    shuffle=True)
 
-loss, accuracy = model.evaluate(validation_data, validation_label, verbose=0)
+loss, accuracy = model.evaluate(test_data, test_labels, verbose=0)
 print("Test: accuracy=%f loss=%f" % (accuracy, loss))
 
 version_name = str(time.time())
 os.makedirs("results/%s" % version_name)
 model.save("model/%s.h5" % version_name)
 
-# Plot training & validation accuracy values
 plt.plot(history.history['acc'])
 plt.plot(history.history['val_acc'])
 plt.title('Model accuracy')
@@ -48,7 +59,6 @@ plt.savefig('results/%s/train.jpg' % version_name)
 
 plt.close()
 
-# Plot training & validation loss values
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
 plt.title('Model loss')
@@ -56,8 +66,3 @@ plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 plt.savefig('results/%s/test.jpg' % version_name)
-
-
-
-
-
